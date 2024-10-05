209b202e396355d239a785d53d89ab180d274279
Lira7682 <a.23308051281191@cbtis128.edu.mx>
2024-10-04 18:46:19 -0600

    Add files via upload

---
+++ precios_pr1.py
@@ -0,0 +1,16 @@
+
+print (" ")#espacio a agregar
+print ("Lira Hernandez Dayana Yamileth:")#datos del realizador
+print ("Indica que precio es menor y cual es mayor")#descripcion del codigo
+print (" ")#espacio a agregar
+
+
+precios = [43, 57, 92, 20, 37, 5, 9]#variables almacenadas para demostrar
+
+menor = min(precios)#calcula el precio menor
+mayor = max(precios)#calcula el precio mayor
+
+print ("El menor precio es:", menor)#imprime resultado del calculo
+print ("El mayor precio es:", mayor)#imprime resultado del calculo
+
+print (" ")#espacio a agregar
